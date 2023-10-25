6db8bad2d2b3fae1f9ce6131251830a5bcd586e4
mateuswesley314@gmail.com <mateuswesley314@gmail.com>
2023-10-25 18:49:07 -0300

    adicionando dependencias do SwaggerConfig

--- src/main/java/com/devweb/prontuario/BaseController.java
+++ src/main/java/com/devweb/prontuario/BaseController.java
@@ -4,6 +4,10 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
+@Api(tags = "BaseController")
 public abstract class
 BaseController<T extends BaseEntity,
                                      Repo extends BaseRepositoryJBDC<T>,
@@ -30,6 +35,7 @@ BaseController<T extends BaseEntity,
     protected S service;
     protected Class<T> entityType;
     protected Class<R> entityResponseDTOType;
+
     public BaseController(M mapper, S service, Class<T> entityType, Class<R> entityResponseDTOType){
         this.mapper = mapper;
         this.service = service;
@@ -38,6 +44,11 @@ BaseController<T extends BaseEntity,
     }
 
     @GetMapping
+    @ApiOperation(value = "Obter uma página de entidades", response = BaseController.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucesso, retorna uma Página"),
+            @ApiResponse(code = 200, message = "Se não existir nenhuma instância, retorna uma página sem conteúdo.")
+    })
     public ResponseEntity<Page<R>> getAll(@PageableDefault( sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
         Page<T> page = service.getAll(pageable);
         List<T> listaEntity = page.getContent();
@@ -47,7 +58,15 @@ BaseController<T extends BaseEntity,
         return ResponseEntity.status(HttpStatus.OK).body(new PageImpl<>(dtoList, pageable, dtoList.size()));
     }
 
+
+
+
     @GetMapping("/{id}")
+    @ApiOperation(value = "Obter uma entidade pelo ID", response = BaseController.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucesso, retorna a entidade solicitada."),
+            @ApiResponse(code = 404, message = "Entidade não encontrada.")
+    })
     public ResponseEntity<R> getById(@PathVariable String id){
         T response = service.getById(id);
         R responseDTO = mapper.entityToResponseDto(response, entityResponseDTOType);
@@ -55,13 +74,24 @@ BaseController<T extends BaseEntity,
     }
 
     @PostMapping
+    @ApiOperation(value = "Criar uma nova instância de entidade.", response = BaseController.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucesso, retorna a entidade criada."),
+            @ApiResponse(code = 400, message = "Existe algum erro na requisição.")
+    })
     public ResponseEntity<Object> create(@RequestBody E dto){
         T entity = mapper.requestDtoToEntity(dto, entityType);
         T created = service.create(entity);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
+
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Criar uma nova instância de entidade.", response = BaseController.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sucesso, retorna a entidade criada."),
+            @ApiResponse(code = 400, message = "Existe algum erro na requisição.")
+    })
     public ResponseEntity<Object> delete(@PathVariable String id){
         service.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
